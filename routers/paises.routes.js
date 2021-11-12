c1c6bb6ff054271ab64365b4d35556b1e00a3923
thiagovieira7 <thiagovieira7@outlook.com>
2021-11-11 21:40:06 -0300

    update

--- routers/paises.routes.js
+++ routers/paises.routes.js
@@ -15,19 +15,24 @@ router.get("/listall", async (req, res) => {
   })
 });
 
-// router.get("/listall/:id", (req, res) => {
-//   const id = req.params.id - 1;
-//   res.status(200).json(lista[id]);
-// });
+router.get("/listall/:id", async (req, res) => {
+  const id = req.params.id - 1;
+  await paises.find({}).then((paises) => {
+    res.status(200).json(paises[id]);
+
+  }).catch((err) => {
+    console.error(err);
+  })
+}); //verificar se esta correto
 
 router.get("/listname/:nome", async (req, res) => {
   const nome = req.params.nome;
-  await pais.findOne({ name: nome }).then((pais) => {//verofocar p name:nome
-    console.log(pais);
-    if (pais == null) {
+  await paises.findOne({ name: nome }).then((paises) => {//verofocar p name:nome
+    console.log(paises);
+    if (paises == null) {
       res.status(404).json({ message: "Não localizado" });
     } else {
-      res.status(200).json(pais);
+      res.status(200).json(paises);
     }
     
   }).catch((err) => {
@@ -38,21 +43,21 @@ router.get("/listname/:nome", async (req, res) => {
 
 router.post("/add", async (req, res) => {
   
-  if (!pais.nome) {
+  if (!paises.nome) {
     res.status(400).json({ message: "NOME DO PAÍS inválido. Certifique-se que o body da requisição possui o NOME correto do (pais)." });
     return;
-  } else if (!pais.populacao) {
+  } else if (!paises.populacao) {
     res.status(400).json({ message: "POPULAÇÃO inválida. Certifique-se de que o body da requisição possui o numero total de habitantes no campo (populacao)." });
     return;
-  } else if (!pais.linguaMae) {
+  } else if (!paises.linguaMae) {
     res.status(400).json({ message: "IDIOMA NATIVO inválida. Certifique-se de que o body da requisição possui a informação da lingua nativa corretamente no campo (linguaMae)." });
     return;
-  } else if (!pais.pib) {
+  } else if (!paises.pib) {
     res.status(400).json({ message: "PIB inválido. Certifique-se de que o body da requisição possui a informação do produto interno bruto no campo (pib)." });
     return;
   }
 
-  await pais.create(req.body).then(() => {
+  await paises.create(req.body).then(() => {
     res.status(200).json({ message: "País cadastrado com sucesso..." })
   }).catch((err) => {
     res.status(400).json({ message: "Erro ao cadastrar" });
@@ -64,7 +69,7 @@ router.post("/add", async (req, res) => {
 
 router.put("/update/:id", (req, res) => {
   const id = req.params.id - 1;
-  if (!pais.nome | !pais.populacao | !pais.linguaMae | !pais.pib) {
+  if (!paises.nome | !paises.populacao | !paises.linguaMae | !paises.pib) {
     res.status(400).json({ message: "Informação para alteração não inserida faltante. Por favor verifique o campo Body da requisição." });
 
     return;
@@ -73,7 +78,7 @@ router.put("/update/:id", (req, res) => {
     return;
   };
 
-  await pais.updateOne({ __id: id }, req.body).then(() => {
+  await paises.updateOne({ __id: id }, req.body).then(() => {
     res.status(200).json({ message: `Dados do país alterados com sucesso: ${lista[id]}` });
   }).catch((er) => {
     console.error(err);
@@ -84,7 +89,7 @@ router.put("/update/:id", (req, res) => {
 
 router.delete("/delete/:id", async (req, res) => {
   if (req.params.id.lenght == 24) {
-    await pais.deleteOne({ __id: req.params.id }).then(() => {
+    await paises.deleteOne({ __id: req.params.id }).then(() => {
       res.status(200).json({ message: "Deletado com sucesso" });
     }).catch((err) => {
       console.error(err);
